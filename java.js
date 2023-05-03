95126f2e6e5b57abcbd90df1ea3bc2e598a67ff4
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2023-05-02 17:14:03 -0700

    adding funtions for the calcultor can do the math problems

---
+++ java.js
@@ -0,0 +1,24 @@
+const a = 5;
+const b = 5;
+const operator = '+';
+
+function add(a, b){
+ return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
