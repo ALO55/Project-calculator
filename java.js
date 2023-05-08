0bef55830241cbb4926adaaf0462ae18a2917b6c
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2023-05-07 17:46:52 -0700

    add the javascript to make the calculator work

--- java.js
+++ java.js
@@ -1,29 +1,86 @@
-const a = document.querySelector('.input');
-const b = 5;
-const operator = '+';
+const input = document.querySelector('.input');
+const output = document.querySelector('.output')
+let a = 0;
+let b = 0;
+let num = '';
+let operator = '';
+let result = 0;
 const number = document.querySelectorAll('.number');
 const div = document.querySelector('#div');
+const mul = document.querySelector('#multiply');
+const sub = document.querySelector('#subtract');
+const ad = document.querySelector('#add');
+const equal = document.querySelector('#equal');
+const clear = document.querySelector('#clear');
 
-
-number.forEach( num =>{
-    num.addEventListener('click', ()=>{
-        a.textContent += num.id;
+//code for choosing numbers
+number.forEach( btn =>{
+    btn.addEventListener('click', ()=>{
+        input.textContent += btn.id;
+        num += btn.id;
     });
 });
 
 
+
+//code for the operator buttons 
+div.addEventListener('click', () => {
+    a = +num;
+    operator = '/';
+    input.textContent += operator;
+    num = '';
+});
+
+mul.addEventListener('click', () => {
+    a = +num;
+    operator = '*';
+    input.textContent += operator;
+    num = '';
+});
+
+sub.addEventListener('click', () => {
+    a = +num;
+    operator = '-';
+    input.textContent += operator;
+    num = '';
+});
+
+ad.addEventListener('click', () => {
+    a = +num;
+    operator = '+';
+    input.textContent += operator;
+    num = '';
+});
+
+
+
+// code to finish the operation
+equal.addEventListener('click', () =>{
+    b = +num;
+    output.textContent = operate(operator, a, b);
+});
+
+
+clear.addEventListener('click', () => {
+    a = '';
+    operator = '';
+    input.textContent = '';
+    output.textContent = '';
+    num = '';
+});
+
 function operate (operator, a, b){
-    if (operator == '+'){
-        add(a, b);
+    if (operator === '+'){
+        return add(a, b);
     }
-    else if(operator == '-'){
-        subtract(a, b);
+    else if(operator === '-'){
+        return subtract(a, b);
     }
-    else if(operator == '*'){
-        multiply(a, b);
+    else if(operator === '*'){
+        return multiply(a, b);
     }
-    else if(operator == '/'){
-        divide(a, b);
+    else if(operator === '/'){
+        return divide(a, b);
     }
 }
 
